cfafcc5084c58317d74beb06ee7af53b3e1654f8
unknown <samirsingh.2000.ss@gmail.com>
2023-07-19 12:32:02 +0530

    Map and Filter PollyFill

--- 33. Map PollyFill.js
+++ 33. Map PollyFill.js
@@ -1,37 +1,17 @@
-//  Basic Version of map polyFill
+// pollyfill for map method
+
 let arr = [1, 2, 3, 4];
 
-function mapPolyFill(arr, cb) {
-  let temp = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    temp.push(cb(arr[i]));
-  }
-
-  return temp;
-}
-
-function multipleTwo(item) {
-  return item * 2;
-}
-
-let newArr = mapPolyFill(arr, multipleTwo);
-
-console.log(newArr);
-
-// Advance version of map polyFill
 Array.prototype.myMap = function (cb) {
   let temp = [];
-
   for (let i = 0; i < this.length; i++) {
-    temp.push(cb(this[i]));
+    temp.push(cb(this[i], i, this));
   }
-
   return temp;
 };
 
-let newArr2 = arr.myMap((item) => {
-  return item * 2;
+let ans = arr.myMap((item) => {
+  return item * 3;
 });
 
-console.log(newArr2);
+console.log(ans);
